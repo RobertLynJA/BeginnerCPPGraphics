ed07959251e1d14980aa7b1cffdc4e5036383ecc
Robert Lyn <lenny4422@gmail.com>
2022-06-02 16:34:44 -0400

    Create a window

--- BeginnerCPPGraphics/main.cpp
+++ BeginnerCPPGraphics/main.cpp
@@ -4,13 +4,29 @@ using namespace std;
 
 int main(int argc, char* args[]) {
 
+	const int SCREEN_WIDTH = 800;
+	const int SCREEN_HEIGHT = 600;
+
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		cout << "SDL init failed." << endl;
 		return 1;
 	}
 
-	cout << "SDL Init succeeded." << endl;
+	SDL_Window* window = SDL_CreateWindow("Particle Fire Explosion",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		SCREEN_WIDTH,
+		SCREEN_HEIGHT,
+		SDL_WINDOW_SHOWN);
 
+	SDL_Delay(3000);
+
+	if (window == nullptr) {
+		SDL_Quit();
+		return 2;
+	}
+
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 
 	return 0;
