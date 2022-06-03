635df14a3a87e1836254e1497796e4dbe66e47db
Robert Lyn <lenny4422@gmail.com>
2022-06-02 20:01:14 -0400

    Clears the screen

--- BeginnerCPPGraphics/main.cpp
+++ BeginnerCPPGraphics/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <SDL.h>
+#include <vector>
 using namespace std;
 
 int main(int argc, char* args[]) {
@@ -18,6 +19,34 @@ int main(int argc, char* args[]) {
 		SCREEN_WIDTH,
 		SCREEN_HEIGHT,
 		SDL_WINDOW_SHOWN);
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RendererFlags::SDL_RENDERER_PRESENTVSYNC);
+
+	if (!renderer) {
+		cout << "Could not create renderer" << endl;
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 2;
+	}
+
+	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB888, SDL_TextureAccess::SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	if (!texture) {
+		cout << "Could not create texture" << endl;
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 2;
+	}
+
+	std::vector<Uint32> buffer(SCREEN_WIDTH * SCREEN_HEIGHT);
+
+	memset(&buffer[0], 0xFFF, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+
+	SDL_UpdateTexture(texture, NULL, &buffer[0], SCREEN_WIDTH * sizeof(Uint32));
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, texture, NULL, NULL);
+	SDL_RenderPresent(renderer);
 
 	bool quit = false;
 	SDL_Event event;
@@ -39,6 +68,8 @@ int main(int argc, char* args[]) {
 		return 2;
 	}
 
+	SDL_DestroyTexture(texture);
+	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
